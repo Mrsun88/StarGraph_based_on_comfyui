fcff94c04d64ba4ef9a35339378fa9ba11a91138
ysjiege2023 <ysjiege2022@163.com>
2025-08-18 17:31:26 +0800

    初始文件

---
+++ src/main/java/cn/jiege/starGraph/StarGraphApp.java
@@ -0,0 +1,12 @@
+package cn.jiege.starGraph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarGraphApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarGraphApp.class, args);
+    }
+}
